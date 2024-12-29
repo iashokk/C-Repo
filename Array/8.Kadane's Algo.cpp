274d57c5c4e002fd0ee2b931a78ce19f694c9cfc
iashokk <ashokkumaravel02@gmail.com>
2024-12-29 21:21:43 +0530

    print array also

--- Array/8.Kadane's Algo.cpp
+++ Array/8.Kadane's Algo.cpp
@@ -5,21 +5,37 @@ int largestSum(int arr[], int n)
 {
     int cs = 0;
     int largest =0;
+    int s =0;
+    int m = 0;
+    int t=0;
     for(int i =0;i<n;i++)
     {
         cs = cs + arr[i];
         if(cs<0)
         {
             cs=0;
+            t = i+1;
         }
-        largest=max(largest,cs);
+       if(cs>largest)
+       {
+           largest = cs;
+           s=t;
+           m=i;
+       }
     }
-    return largest;
+    for(int i=s;i<=m;i++)
+    {
+        cout<<arr[i];
+    
+    }
+    cout<<endl;
+    cout<<largest;
+    return 0;
 }
 
 int main()
 {
-    int arr[]={1,2,3,4,5};
+    int arr[]={1,2,3,-4,5};
     int n = 5;
-   cout<<largestSum(arr,n);
+largestSum(arr,n);
 }
